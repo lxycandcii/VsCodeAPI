471338109c36d3d436869ea1439b775b6b5a58b0
lxycandcii <lxycandcii@163.com>
2019-04-12 10:30:45 +0800

    M1

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VsCodeAPI.Entity;
+
+namespace VsCodeAPI.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+
+        /// <summary>
+        /// 图片
+        /// </summary>
+        //public DbSet<Photo> Photos { get; set; }
+
+        /// <summary>
+        /// 图片
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasDefaultValueSql("Newid()");
+                //entity.Property(e => e.IsDelete)
+                //    .HasDefaultValue(IsDeleted.No);
+                //entity.Property(e => e.IsLocked)
+                //    .HasDefaultValue(IsLocked.UnLocked);
+                //entity.Property(e => e.Role)
+                //    .HasDefaultValue(UserRole.Guest);
+                //entity.Property(e => e.CreatedAt)
+                //    .ValueGeneratedOnAdd()
+                //    .HasDefaultValueSql("getdate()");
+                //entity.Property(e => e.LastUpdatedAt)
+                //    .ValueGeneratedOnAddOrUpdate()
+                //    .HasDefaultValueSql("getdate()");
+            });
+        }
+    }
+}
---
+++ Entity/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VsCodeAPI.Entity
+{
+    public class User
+    {
+        [Key]
+        public Guid? Id { get; set; } // = Guid.NewGuid();
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+    }
+}
